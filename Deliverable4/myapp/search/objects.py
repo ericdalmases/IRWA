ff57a0688407aeff61eca36d8f3f38e9161e14b9
marcagve18 <78203527+marcagve18@users.noreply.github.com>
2023-11-16 15:18:33 +0100

    Revert "updates project template"
    This reverts commit d2163d92cfcb39a56cf83c71f1c1cffcb3b8a7b2.

--- Deliverable4/myapp/search/objects.py
+++ Deliverable4/myapp/search/objects.py
@@ -1,14 +1,20 @@
 import json
 
+
 class Document:
     """
     Original corpus data as an object
     """
 
-    def __init__(self, id, quote,author):
+    def __init__(self, id, title, description, doc_date, likes, retweets, url, hashtags):
         self.id = id
-        self.quote = quote
-        self.author = author
+        self.title = title
+        self.description = description
+        self.doc_date = doc_date
+        self.likes = likes
+        self.retweets = retweets
+        self.url = url
+        self.hashtags = hashtags
 
     def to_json(self):
         return self.__dict__
@@ -19,9 +25,32 @@ class Document:
         """
         return json.dumps(self)
 
-class ResultItem:
-    def __init__(self, id, quote, author,ranking):
+
+class StatsDocument:
+    """
+    Original corpus data as an object
+    """
+
+    def __init__(self, id, title, description, doc_date, url, count):
         self.id = id
-        self.quote = quote
-        self.author = author
+        self.title = title
+        self.description = description
+        self.doc_date = doc_date
+        self.url = url
+        self.count = count
+
+    def __str__(self):
+        """
+        Print the object content as a JSON string
+        """
+        return json.dumps(self)
+
+
+class ResultItem:
+    def __init__(self, id, title, description, doc_date, url, ranking):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.doc_date = doc_date
+        self.url = url
         self.ranking = ranking
