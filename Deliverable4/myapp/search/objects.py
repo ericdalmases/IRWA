d2163d92cfcb39a56cf83c71f1c1cffcb3b8a7b2
marcagve18 <78203527+marcagve18@users.noreply.github.com>
2023-11-16 15:13:35 +0100

    updates project template

--- Deliverable4/myapp/search/objects.py
+++ Deliverable4/myapp/search/objects.py
@@ -1,20 +1,14 @@
 import json
 
-
 class Document:
     """
     Original corpus data as an object
     """
 
-    def __init__(self, id, title, description, doc_date, likes, retweets, url, hashtags):
+    def __init__(self, id, quote,author):
         self.id = id
-        self.title = title
-        self.description = description
-        self.doc_date = doc_date
-        self.likes = likes
-        self.retweets = retweets
-        self.url = url
-        self.hashtags = hashtags
+        self.quote = quote
+        self.author = author
 
     def to_json(self):
         return self.__dict__
@@ -25,32 +19,9 @@ class Document:
         """
         return json.dumps(self)
 
-
-class StatsDocument:
-    """
-    Original corpus data as an object
-    """
-
-    def __init__(self, id, title, description, doc_date, url, count):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.doc_date = doc_date
-        self.url = url
-        self.count = count
-
-    def __str__(self):
-        """
-        Print the object content as a JSON string
-        """
-        return json.dumps(self)
-
-
 class ResultItem:
-    def __init__(self, id, title, description, doc_date, url, ranking):
+    def __init__(self, id, quote, author,ranking):
         self.id = id
-        self.title = title
-        self.description = description
-        self.doc_date = doc_date
-        self.url = url
+        self.quote = quote
+        self.author = author
         self.ranking = ranking
--- Deliverable4/myapp/search/search_engine.py
+++ Deliverable4/myapp/search/search_engine.py
@@ -3,23 +3,26 @@ import random
 from myapp.search.objects import ResultItem, Document
 
 
-def build_demo_results(corpus: dict, search_id):
+def build_demo_results(corpus: dict, search_id,search_query):
     """
     Helper method, just to demo the app
     :return: a list of demo docs sorted by ranking
     """
     res = []
     size = len(corpus)
-    ll = list(corpus.values())
-    for index in range(random.randint(0, 40)):
-        item: Document = ll[random.randint(0, size)]
-        res.append(ResultItem(item.id, item.title, item.description, item.doc_date,
-                              "doc_details?id={}&search_id={}&param2=2".format(item.id, search_id), random.random()))
 
-    # for index, item in enumerate(corpus['Id']):
-    #     # DF columns: 'Id' 'Tweet' 'Username' 'Date' 'Hashtags' 'Likes' 'Retweets' 'Url' 'Language'
-    #     res.append(DocumentInfo(item.Id, item.Tweet, item.Tweet, item.Date,
-    #                             "doc_details?id={}&search_id={}&param2=2".format(item.Id, search_id), random.random()))
+    ll = list(corpus.values())
+
+    for index in range(0, 40):
+        item: Document = ll[index] #getting a random object in the list of documents
+
+        #if you want to search if the search query is in the quote
+        if search_query in item.quote.split():
+            res.append(ResultItem(item.id, item.quote,item.author,random.randint(0, size)))
+
+        # id, quote, author,ranking
+        #generate total random results
+        #res.append(ResultItem(item.id, item.quote,item.author,random.randint(0, size)))
 
     # simulate sort by ranking
     res.sort(key=lambda doc: doc.ranking, reverse=True)
@@ -34,7 +37,7 @@ class SearchEngine:
 
         results = []
         ##### your code here #####
-        results = build_demo_results(corpus, search_id)  # replace with call to search algorithm
+        results = build_demo_results(corpus, search_id,search_query)  # replace with call to search algorithm
 
         # results = search_in_corpus(search_query)
         ##### your code here #####
